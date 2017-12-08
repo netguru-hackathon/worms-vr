4c37b2d3fbdc0aab76dacd34b83fb07ad440847a
Artur Okoński <okonski.a.dev@gmail.com>
2017-12-08 16:19:45 +0100

    Emit gyroscope data

--- public/client.js
+++ public/client.js
@@ -124,3 +124,12 @@ var animate = function () {
 };
 
 animate();
+
+window.addEventListener("deviceorientation", function(event) {
+  var gyro_data = {
+    alpha: event.alpha,
+    beta: event.beta,
+    gamma: event.gamma
+  }
+  socket.emit('gyro_data', gyro_data);
+}, true);
